6a02bc7a234524ec4ad39824d5e62d4242f6a1dc
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-11-05 23:19:53 +0800

    test if can compile with cedar-policy
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use cedar_policy::{Authorizer, Context, Entities, PolicySet, Request};
 use std::sync::{Arc, RwLock};
 use tokio::time::{sleep, Duration};
 
@@ -55,10 +56,43 @@ impl Jwt {
     }
 }
 
+fn run_cedar() {
+    println!("Testing Cedar:");
+
+    const POLICY_SRC: &str = r#"
+permit(principal == User::"alice", action == Action::"view", resource == File::"93");
+"#;
+    let policy: PolicySet = POLICY_SRC.parse().unwrap();
+
+    let action = r#"Action::"view""#.parse().unwrap();
+
+    let alice = r#"User::"alice""#.parse().unwrap();
+    let file = r#"File::"93""#.parse().unwrap();
+    let request = Request::new(alice, action, file, Context::empty(), None).unwrap();
+
+    let entities = Entities::empty();
+    let authorizer = Authorizer::new();
+    let answer = authorizer.is_authorized(&request, &policy, &entities);
+
+    // Should output `Allow`
+    println!("{:?}", answer.decision());
+
+    let action = r#"Action::"view""#.parse().unwrap();
+    let bob = r#"User::"bob""#.parse().unwrap();
+    let file = r#"File::"93""#.parse().unwrap();
+    let request = Request::new(bob, action, file, Context::empty(), None).unwrap();
+
+    let answer = authorizer.is_authorized(&request, &policy, &entities);
+
+    // Should output `Deny`
+    println!("{:?}", answer.decision());
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let cedarling = Cedarling::new(AuthzConfig, JwtConfig);
     println!("cedarling initialized");
     cedarling.authz().await;
     println!("authz done");
+    run_cedar();
 }
