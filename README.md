1344567969a14160ba1e2de346249fd6919a3196
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-11-05 23:08:14 +0800

    allow compilation to WASM
    Signed-off-by: rmarinn <34529290+rmarinn@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+## Add The WASM Target:
+
+```sh
+rustup target add wasm32-unknown-unknown
+```
+
+## Compile to WASM
+
+```sh
+cargo build --target wasm32-unknown-unknown
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::sync::mpsc::Receiver;
-use std::sync::{mpsc, Arc, RwLock};
-use threadpool::ThreadPool;
-use tokio::runtime::Runtime;
+use std::sync::{Arc, RwLock};
 use tokio::time::{sleep, Duration};
 
 #[allow(dead_code)]
@@ -12,14 +9,13 @@ struct Cedarling {
 
 impl Cedarling {
     fn new(authz_conf: AuthzConfig, jwt_conf: JwtConfig) -> Self {
-        let jwt_rx = Jwt::new(jwt_conf);
-        println!("started jwt init");
-
+        let jwt = Jwt::new(jwt_conf);
         let authz = Authz::new(authz_conf);
-        println!("authz initialized");
-
-        let jwt = jwt_rx.recv().expect("failed to receive JwtService");
         Self { authz, jwt }
+    }
+
+    async fn authz(&self) {
+        self.jwt.validate_tokens().await;
     }
 }
 
@@ -35,46 +31,34 @@ impl Authz {
 #[allow(dead_code)]
 struct Jwt {
     config: JwtConfig,
-    pool: ThreadPool,
     jwks: Arc<RwLock<Option<String>>>,
 }
 struct JwtConfig;
 
 impl Jwt {
-    fn new(config: JwtConfig) -> Receiver<Self> {
-        let (tx, rx) = mpsc::channel();
+    fn new(config: JwtConfig) -> Self {
+        Self {
+            config,
+            jwks: Arc::new(RwLock::new(None)),
+        }
+    }
 
-        let pool = ThreadPool::new(4);
-        let jwks = Arc::new(RwLock::new(None));
-        pool.clone().execute(move || {
-            let runtime = Runtime::new().unwrap();
+    async fn validate_tokens(&self) {
+        // simulate network delay
+        println!("sending http request");
+        sleep(Duration::from_millis(1000)).await;
+        println!("received http response");
 
-            // asnyc calls get isolated here
-            runtime.block_on(async {
-                fetch_jwks(jwks.clone()).await;
-            });
-
-            tx.send(Self { config, pool, jwks })
-                .expect("error sending result");
-        });
-
-        rx
+        // update local jwks
+        let mut jwks = self.jwks.write().expect("failed to obtain write lock");
+        *jwks = Some("jwks".to_string());
     }
 }
 
-// simulate network request
-async fn fetch_jwks(jwks: Arc<RwLock<Option<String>>>) {
-    // simulate network delay
-    println!("sending http request");
-    sleep(Duration::from_millis(1000)).await;
-    println!("received http response");
-
-    // update local jwks
-    let mut jwks_lock = jwks.write().expect("failed to obtain write lock");
-    *jwks_lock = Some("jwks".to_string());
-}
-
-fn main() {
-    let _cedarling = Cedarling::new(AuthzConfig, JwtConfig);
-    println!("cedarling initialized")
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let cedarling = Cedarling::new(AuthzConfig, JwtConfig);
+    println!("cedarling initialized");
+    cedarling.authz().await;
+    println!("authz done");
 }
